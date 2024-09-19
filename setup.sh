d5139f7b8e20ff5f79de509fad030f313f855b17
lovinduw <lovinduw@hotmail.com>
2024-09-19 19:26:00 +0200

    bin/bash added

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+# Create virtual environment
 python -m venv myenv
+
+# Activate virtual environment
 myenv/Scripts/activate
+
+# Install fine library with from GitHub with all the dependencies
 pip install git+https://github.com/lovinduw/mga_in_fine.git#egg=fine
